56175fb063ca2816fdc2d37524e2cf93ff8ceef3
Micah Walter <micahjwalter@gmail.com>
2018-10-22 20:22:59 -0400

    Rearrangement of Add File dialog box

--- src/js/templates/addfile-tpl.js
+++ src/js/templates/addfile-tpl.js
@@ -21,27 +21,25 @@ let addfile_tpl = `
   <h4 class="mdl-dialog__title">Add score</h4>
   <div class="mdl-dialog__content">
     <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
-      <div class="mdl-tabs__tab-bar">
-          <a href="#crim-panel" class="mdl-tabs__tab is-active">From CRIM</a>
-          <a href="#web-panel" class="mdl-tabs__tab">Web URL</a>
-      </div>
-      <div class="mdl-tabs__panel is-active" id="crim-panel">
-        {{#each scores}}
-          <div style="display: block;">
-            <input type="checkbox" id="s_{{this.piece_id}}" class="mdl-checkbox__input" value="{{this.url}}" data-composer="{{this.composer}}" data-title="{{this.title}}" data-piece_id="{{this.piece_id}}" />
-            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" style="display: inline;" for="s_{{this.piece_id}}">
-              [{{this.piece_id}}] {{this.title}} ({{this.composer}})
-            </label>
-          </div>
-        {{/each}}
-      </div>
       <div class="mdl-tabs__panel" id="web-panel">
         <form action="#">
           <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-            <input class="mdl-textfield__input" type="text" id="url_input" value="http://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei">
-            <label class="mdl-textfield__label" for="url_input">URL...</label>
+            <label class="block mdl-textfield__label" for="url_input">
+              URL:
+              <input style="width:80%;" class="inline mdl-textfield__input" type="text" id="url_input" value="http://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei">
+            </label>
           </div>
         </form>
+      </div>
+      <div class="mdl-tabs__panel is-active" id="crim-panel">
+        <p>
+          {{#each scores}}
+            <label class="block mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="s_{{this.piece_id}}">
+              <input type="checkbox" id="s_{{this.piece_id}}" class="inline mdl-checkbox__input" value="{{this.url}}" data-composer="{{this.composer}}" data-title="{{this.title}}" data-piece_id="{{this.piece_id}}" />
+              [{{this.piece_id}}] {{this.title}} ({{this.composer}})
+            </label>
+          {{/each}}
+        </p>
       </div>
     </div>
   </div>
