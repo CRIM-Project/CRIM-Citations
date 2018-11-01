b64c6a1e1262add5d755621d53154bba8fa177ff
Micah Walter <micahjwalter@gmail.com>
2018-11-01 08:32:30 -0400

    Prevent scrunching of scores; account for taller header

--- src/js/templates/score-tpl.js
+++ src/js/templates/score-tpl.js
@@ -4,27 +4,25 @@ let pag_tpl = `
 <div class="score_header">
   <hr />
   <h2>{{title}} <small>{{piece_id}}</small></h2>
-</div>
-<h3>{{composer}}</h3>
-<div class="row-fluid">
-  <div class="span9">
-    <button type="button" class="btn mdl-button mdl-button--attention mdl-js-button btn-danger close_score_button">
-      Remove piece
-    </button>
-    <button type="button" class="btn btn-primary mdl-button mdl-js-button show-score-relationship" style="display:none">
-      New relationship
-    </button>
-    <button type="button" class="btn mdl-button mdl-js-button mdl-button--colored show-score-observations" style="display:none">
-      Show relationships
-    </button>
-  </div>
-  <div class="pull-right">
-    <button type="button" class="btn mdl-button mdl-js-button collapse_expand_button">
-      <span class="collapse">Collapse</span>
-      <span class="expand" style="display:none;">Expand</span>
-    </button>
-  </div>
-  <nav class="mdl-navigation">
+  <h3>{{composer}}</h3>
+  <div class="row-fluid">
+    <div class="span9">
+      <button type="button" class="btn mdl-button mdl-button--attention mdl-js-button btn-danger close_score_button">
+        Remove piece
+      </button>
+      <button type="button" class="btn btn-primary mdl-button mdl-js-button show-score-relationship" style="display:none">
+        New relationship
+      </button>
+      <button type="button" class="btn mdl-button mdl-js-button mdl-button--colored show-score-observations" style="display:none">
+        Show relationships
+      </button>
+    </div>
+    <div class="pull-right">
+      <button type="button" class="btn mdl-button mdl-js-button collapse_expand_button">
+        <span class="collapse">Collapse</span>
+        <span class="expand" style="display:none;">Expand</span>
+      </button>
+    </div>
     <div class="btn-group score_pagination pull-right" role="group">
       <button type="button" class="btn prevPage mdl-button mdl-js-button score_collapsible">
         &lt;
@@ -33,7 +31,7 @@ let pag_tpl = `
         &gt;
       </button>
     </div>
-  </nav>
+  </div>
 </div>
 <div class="score score_collapsible"></div>
 `
